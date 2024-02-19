685e00926d096b940adee221e0896b8cd2cb6182
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-02-19 20:20:39 +0100

    feat:iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur chef", "UltraPro Chef" ]
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String
+  },
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "others"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min : 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+ 
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
